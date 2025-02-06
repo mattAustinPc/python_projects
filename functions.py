42fec923881ac6f234d647b53ca18985cc4f1a29
Matt Austin <matthewaustin@utilita.co.uk>
2025-02-06 13:25:20 +0000

    pypro: tidy up savings annual_summary

--- functions.py
+++ functions.py
@@ -5,12 +5,10 @@ def saving_annual_summary(
     """ Provide annual projection of savings based on monthly saving amount. """
 
     interest_month_rate = (interest_rate/100)/12
-    interest_month_value = 0
     interest_total_value = 0
-    savings_added = 0
     savings_total = initial_savings
     saving_summary = "Month|Interest|Interest Total|Saving Added|Saving Total\n"
-    saving_line = ""
+
     for month in range(1,13):
         interest_month = interest_month_rate * savings_total
         interest_total_value += interest_month
@@ -23,6 +21,5 @@ def saving_annual_summary(
             f"{str(round(interest_total_value,2)).rjust(14)}|"
             f"{str(round(savings_added,2)).rjust(12)}|"
             f"{str(round(savings_total,2)).rjust(12)}\n")
-            #print(saving_line)
             saving_summary += saving_line
     return saving_summary
