eda3e4b4463ee6332f32bb8927ad8a91db49a31e
Matt Austin <matthewaustin@utilita.co.uk>
2025-02-08 14:59:34 +0000

    pypro: add string functions and refactor report to generic interest

--- finance/functions.py
+++ finance/functions.py
@@ -1,24 +1,39 @@
-def saving_annual_summary(
-        monthly_saving: float,
+from typing import Any
+
+def money(money_flt: float, pad: int = 0) -> str:
+    """ Convert float to formatted string with option to right pad"""
+    return f"{money_flt:>{pad},.2f}"
+
+def money_list(money_items: Any, pad: int = 0, sep :str = "," ) -> str:
+    """ Convert multiple floats to formatted string with optional right padding and seperator"""
+    return sep.join([f"{money(money_item,pad)}" for money_item in money_items])
+
+def get_longest_string(strings: tuple)->int:
+    """ get longest string length from iterable. """
+    string_list = sorted(strings,key=len, reverse=True)
+    return len(string_list[0])
+
+def rpad_tup(tup: tuple,pad: int)->tuple:
+    """ Right justify tuple to length of longest string element. """
+    return tuple([tup_string.rjust(pad) for tup_string in tup])
+
+def monthly_interest_report(
         interest_rate: float,
-        initial_savings: float = 0)->str:
+        monthly_transaction: float=0,
+        initial_amount: float = 0,
+        months: int = 12,
+        ) -> list:
     """ Provide annual projection of savings based on monthly saving amount. """
-
-    interest_month_rate = (interest_rate/100)/12
-    interest_total_value = 0
-    savings_total = initial_savings
-    saving_summary = "Month|Interest|Interest Total|Saving Added|Saving Total\n"
-
-    for month in range(1,13):
-        interest_month = interest_month_rate * savings_total
-        interest_total_value += interest_month
-        savings_total += (interest_month + monthly_saving)
-        savings_added = savings_total - initial_savings
-        if (month % 3) == 0:
-            saving_summary += (
-            f"{str(month).rjust(5)}|"
-            f"{str(round(interest_month,2)).rjust(8)}|"
-            f"{str(round(interest_total_value,2)).rjust(14)}|"
-            f"{str(round(savings_added,2)).rjust(12)}|"
-            f"{str(round(savings_total,2)).rjust(12)}\n")
-    return saving_summary
+    interest_report: list = []
+    interest_month_rate = (interest_rate / 100) / 12
+    total_interest = 0.0
+    total_value: float = round(initial_amount,2)
+    for month in range(1,months+1):
+        interest_month = round(interest_month_rate * total_value,2)
+        total_interest += interest_month
+        total_value = round(interest_month + monthly_transaction + total_value,2)
+        added_value =  round(total_value-initial_amount,2)
+        savings = dict(month=month, added_interest=interest_month, total_interest=total_interest,added_value= added_value,
+                       total_value=total_value)
+        interest_report.append(savings)
+    return interest_report
--- finance/savings_printout.py
+++ finance/savings_printout.py
@@ -1,6 +1,12 @@
-from functions import saving_annual_summary
+from functions import monthly_interest_report
+
 month_amount = 100
 interest_rate = 4.01
-saving_report = saving_annual_summary(month_amount,interest_rate,0)
-print(f"A monthly saving of £{month_amount}, with annual interest of {interest_rate}%"
-      f" will produce a saving of:\n{saving_report}")
+saving = 30000
+print(f"A monthly saving of £{month_amount:,.2f} on top of £{saving:,.2f}, with annual interest of {interest_rate}%"
+      f" will produce savings of:\n")
+
+report = monthly_interest_report(interest_rate, month_amount,interest_rate,12)
+
+for item in report:
+    print(item)
